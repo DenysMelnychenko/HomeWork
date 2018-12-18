e4606eba2513fc42ff22377c0d4c4a75aaf4c7f1
DenysMelnychenko <aspirant89@gmail.com>
2018-12-19 00:43:42 +0200

    Fixed warning according to comments

--- src/main/java/homeworks/PowerAdapter.java
+++ src/main/java/homeworks/PowerAdapter.java
@@ -1,37 +1,35 @@
 package homeworks;
 
 public class PowerAdapter {
-	private boolean status;
-	private short inputVoltage;
-	private FirstOutputVoltage output1 = FirstOutputVoltage.OFF;
-	private SecondOutputVoltage output2 = SecondOutputVoltage.OFF;
+  private boolean status;
+  private short inputVoltage;
+  private FirstOutputVoltage output1 = FirstOutputVoltage.OFF;
+  private SecondOutputVoltage output2 = SecondOutputVoltage.OFF;
 
-	public void showInfo() {
+  public void showInfo() {
+    System.out.printf("%4$s inputVoltage: %1$d output1Voltage: %2$d output2Voltage: %3$d %5$s",
+        inputVoltage, output1.getValue(), output2.getValue(), (status) ? "Turn On" : "Turn Off",
+        (inputVoltage > 250) ? "Hight Voltage" : ((inputVoltage < 200) ? "Low Voltage" : "Start"));
+  }
 
-		System.out.printf("%4$s inputVoltage:%1$3d output1Voltage: %2$2d output2Voltage: %3$2d", inputVoltage,
-				output1.getValue(), output2.getValue(), (status) ? "Turn On" : "Turn Off");
-	}
+  public String getInfo() {
+    return String.format("%4$s inputVoltage: %1$d output1Voltage: %2$d output2Voltage: %3$d %5$s",
+        inputVoltage, output1.getValue(), output2.getValue(), (status) ? "Turn On" : "Turn Off",
+        (inputVoltage > 250) ? "Hight Voltage" : ((inputVoltage < 200) ? "Low Voltage" : "Start"));
 
-	public String getInfo() {
+  }
 
-		return String.format("%4$s inputVoltage: %1$d output1Voltage: %2$d output2Voltage: %3$d", inputVoltage,
-				output1.getValue(), output2.getValue(), (status) ? "Turn On" : "Turn Off");
-
-	}
-
-	public void changeInputVoltage(short value) {
-		if (value > 200 && value < 250) {
-			this.status = true;
-			inputVoltage = value;
-			output1 = FirstOutputVoltage.ON;
-			output2 = SecondOutputVoltage.ON;
-		} else {
-			status = false;
-			inputVoltage = 0;
-			output1 = FirstOutputVoltage.OFF;
-			output2 = SecondOutputVoltage.OFF;
-
-			System.out.println((value > 250) ? "Hight Voltage" : ((value < 200) ? "Low Voltage" : "Start"));
-		}
-	}
+  public void changeInputVoltage(short value) {
+    if (value > 200 && value < 250) {
+      this.status = true;
+      inputVoltage = value;
+      output1 = FirstOutputVoltage.ON;
+      output2 = SecondOutputVoltage.ON;
+    } else {
+      status = false;
+      inputVoltage = value;
+      output1 = FirstOutputVoltage.OFF;
+      output2 = SecondOutputVoltage.OFF;
+    }
+  }
 }
--- src/test/java/homeworks/PowerAdapterTest.java
+++ src/test/java/homeworks/PowerAdapterTest.java
@@ -3,42 +3,45 @@ package homeworks;
 
 
 import static org.junit.Assert.assertEquals;
-
 import org.junit.Before;
 import org.junit.Test;
 
 public class PowerAdapterTest {
 
-	private PowerAdapter pw;
-	
-	@Before
-	public void initialize() {
-		pw = new PowerAdapter();
-	}
+  private PowerAdapter pw;
 
-	@Test
-	public void IfInputVoltageIsZeroShouldBeEqual() {
-		assertEquals("Turn Off inputVoltage: 0 output1Voltage: 0 output2Voltage: 0", pw.getInfo());
-	}
-	
-	@Test
-	public void IfInputvoltageChangeTo220ThanSouldWorkFine() {
-		pw.changeInputVoltage((short)220);
-		assertEquals("Turn On inputVoltage: 220 output1Voltage: 12 output2Voltage: 5", pw.getInfo());
-		
-	}
-	
-	@Test
-	public void IfInputvoltageChangeTo190ThanSouldTurnOff() {
-		pw.changeInputVoltage((short)190);
-		assertEquals("Turn Off inputVoltage: 0 output1Voltage: 0 output2Voltage: 0", pw.getInfo());
-		
-	}
-	
-	@Test
-	public void IfInputvoltageChangeTo2750ThanSouldTurnOff() {
-		pw.changeInputVoltage((short)2750);
-		assertEquals("Turn Off inputVoltage: 0 output1Voltage: 0 output2Voltage: 0", pw.getInfo());
-		
-	}
+  @Before
+  public void initialize() {
+    pw = new PowerAdapter();
+  }
+
+  @Test
+  public void IfInputVoltageIsZeroShouldBeEqual() {
+    assertEquals("Turn Off inputVoltage: 0 output1Voltage: 0 output2Voltage: 0 Low Voltage",
+        pw.getInfo());
+  }
+
+  @Test
+  public void IfInputvoltageChangeTo220ThanSouldWorkFine() {
+    pw.changeInputVoltage((short) 220);
+    assertEquals("Turn On inputVoltage: 220 output1Voltage: 12 output2Voltage: 5 Start",
+        pw.getInfo());
+
+  }
+
+  @Test
+  public void IfInputvoltageChangeTo190ThanSouldTurnOff() {
+    pw.changeInputVoltage((short) 190);
+    assertEquals("Turn Off inputVoltage: 190 output1Voltage: 0 output2Voltage: 0 Low Voltage",
+        pw.getInfo());
+
+  }
+
+  @Test
+  public void IfInputvoltageChangeTo2750ThanSouldTurnOff() {
+    pw.changeInputVoltage((short) 2750);
+    assertEquals("Turn Off inputVoltage: 2750 output1Voltage: 0 output2Voltage: 0 Hight Voltage",
+        pw.getInfo());
+
+  }
 }
