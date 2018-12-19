3e84820ee4116cd30ded7295983631e5da2f8f24
DenysMelnychenko <aspirant89@gmail.com>
2018-12-19 11:06:10 +0200

    Added Basic structure to programm

--- src/main/java/homeworks/Battle.java
+++ src/main/java/homeworks/Battle.java
@@ -3,28 +3,28 @@ package homeworks;
 public class Battle {
 
   public static void main(String[] args) {
-    Hero hero1 = new Mag(randomStatsGenerator(1000), randomStatsGenerator(1000),
-        randomStatsGenerator(1000), randomStatsGenerator(1000));
-    Hero hero2 = new Marksmen(randomStatsGenerator(1000), randomStatsGenerator(1000),
-        randomStatsGenerator(1000), randomStatsGenerator(1000));
+    Hero hero1 = new Mag(randomStatsGenerator(100), randomStatsGenerator(100),
+        randomStatsGenerator(100), randomStatsGenerator(1000));
+    Hero hero2 = new Marksmen(randomStatsGenerator(100), randomStatsGenerator(100),
+        randomStatsGenerator(100), randomStatsGenerator(1000));
     Item wand = new Item();
     Item cap = new Item();
     Item bow = new Item();
     Item boots = new Item();
 
-    while (hero1.getHealth() >= 0 && hero1.getHealth() >= 0F) {
-      hero2.setHealth(hero1.getStrike());
+    while (hero1.getHealth() > 0 && hero2.getHealth() > 0) {
+      hero2.setHealth(hero2.getHealth() - hero1.getStrike());
       try {
         Thread.sleep(1000);
-      } catch (InterruptedException e) {
         System.out.println(hero2.toString());
+      } catch (InterruptedException e) {
         e.printStackTrace();
       }
-      hero1.setHealth(hero2.getStrike());
+      hero1.setHealth(hero1.getHealth() - hero2.getStrike());
       try {
         Thread.sleep(1000);
+        System.out.println(hero1.toString());
       } catch (InterruptedException e) {
-        System.out.println(hero2.toString());
         e.printStackTrace();
       }
     }
