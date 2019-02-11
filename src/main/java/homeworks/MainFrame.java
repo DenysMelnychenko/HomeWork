a75db869d7d36265ec32122d3cca3990869cd12e
DenysMelnychenko <aspirant89@gmail.com>
2019-02-11 23:05:25 +0200

    creates App and MainFrame classes to start my aplication

---
+++ src/main/java/homeworks/MainFrame.java
@@ -0,0 +1,14 @@
+package homeworks;
+
+public class MainFrame extends javax.swing.JFrame {
+
+  private static final long serialVersionUID = 2577622247034082L;
+
+  public MainFrame() {
+    super("File Manager");
+    setSize(500, 500);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setVisible(true);
+
+  }
+}
